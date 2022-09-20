fc9f1104cd3918db2463c36cc03dc8af40cd2583
Mahima Sindal <mahimasindal@gmail.com>
2022-09-20 12:50:28 +0530

    new change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home-here</Link>
+                <Link className="nav-link active" aria-current="page" to="/">Home-changing in master</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/business">Business</Link>
