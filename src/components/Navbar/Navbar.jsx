69ae1f8e374b2bd6909d833af3c37673227972f2
Mahima Sindal <mahimasindal@gmail.com>
2022-09-20 13:06:50 +0530

    commt ticket10

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <Link className="nav-link active" aria-current="page" to="/">Home-changing in master</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/business">Business</Link>
+                <Link className="nav-link" to="/business">Business ticket10 changes</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/entertainment">Entertainment</Link>
