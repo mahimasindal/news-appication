16829e8a7bac46803263e8425d2e4b7df103a3e0
Mahima Sindal <mahimasindal@gmail.com>
2022-09-21 23:03:53 +0530

    added prince

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">NewsMonkey for removing commit</Link>
+          <Link className="navbar-brand" to="/">NewsMonkey for removing commit prince</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
